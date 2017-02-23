ae600f6d23337a4ac50ea98b0c9da395e0252bd6
Vernon <me@example.com>
2017-02-23 14:22:38 -0600

    Completed assignment

--- js/characterCtrl.js
+++ js/characterCtrl.js
@@ -1,3 +1,9 @@
 angular.module("characterApp").controller("characterCtrl", function($scope, characterService) {
     console.log("Working")
+    characterService.getCharacter().then(
+      function(result) {
+        
+        $scope.characters = result
+      }
+    )
 })
--- js/characterService.js
+++ js/characterService.js
@@ -1,3 +1,51 @@
-angular.module("characterApp").service("characterService", function() {
+angular.module("characterApp").service("characterService", function($http, $q) {
     console.log("Service Working")
+
+    this.getCharacter = function() {
+      var deferred = $q.defer()
+      var characters = []
+
+      $http({
+        method: 'GET',
+        url: 'http://pokeapi.co/api/v2/pokemon/1/'
+      }).then(function(result) {
+        characters.push(result.data)
+        if(characters.length === 2) {
+          deferred.resolve(characters)
+        }
+      })
+
+      $http.get('http://pokeapi.co/api/v2/pokemon/2/').then(function(result){
+        characters.push(result.data)
+        if (characters.length === 2) {
+          deferred.resolve(characters)
+        }
+      })
+
+      return deferred.promise
+    }
+
+    // this.getCharacter = function() {
+    //
+    //   var deferred = $q.defer() // write this first
+    //   var characters = [];
+    //     $http({}).then(//push to characters
+    //       //check length of characters, if length === 2
+    //     ) //character 1
+    //     $http({}).then(//push to characters
+    //       //check length of characters, if length === 2
+    //     ) //character 2
+    //   return deferred.promise // write this second to return correct item
+    // }
+    //
+    // this.getCharacter = function() {
+    //     // return $http({
+    //     //   method:'GET',
+    //     //   url: 'http://pokeapi.co/api/v2/pokemon/1/'
+    //     // })
+    //
+    //     return $http.get('http://pokeapi.co/api/v2/pokemon/1/')
+    //
+    // }
+
 })
